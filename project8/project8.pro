1f495d64fe3646e08693539a22e9ab7b0ea759a0
drakekr <kelven@netsgo.com>
2015-03-01 15:37:47 +0900

    radiobutton

---
+++ project8/project8.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-03-01T15:15:31
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = project8
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
+
+FORMS    += widget.ui
---
+++ project8/widget.cpp
@@ -0,0 +1,45 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    singer = "";
+    if(ui->radioButton->isChecked())
+        age = "10대";
+    if(ui->radioButton_2->isChecked())
+        age = "20대";
+    if(ui->radioButton_3->isChecked())
+        age = "30대";
+    if(ui->radioButton_4->isChecked())
+        age = "40대";
+    if(ui->radioButton_5->isChecked())
+        age = "외계인";
+    if(ui->checkBox->isChecked())
+        singer += "소녀시대 ";
+    if(ui->checkBox_2->isChecked())
+        singer += "2ne1 ";
+    if(ui->checkBox_3->isChecked())
+        singer += "이선희 ";
+    if(ui->checkBox_4->isChecked())
+        singer += "걸스데이 ";
+    if(ui->checkBox_5->isChecked())
+        singer += "EXO ";
+    ui->lineEdit->setText(age + "인 당신은 " + singer +"를 좋아하네요.");
+}
+
+void Widget::on_pushButton_2_clicked()
+{
+    this->close();
+}
